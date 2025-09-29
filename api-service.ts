62a06b0b0da310c06de1cb5c40524d39de281c0f
Sebastian Villa <80434951+Sebasdsk@users.noreply.github.com>
2025-09-29 13:59:24 -0700

    Componente proyectos
    Se creo el componente proyecto, el cual es un CRUD y se modifico el archivo api-service para que este funcione de manera correcta

---
+++ api-service.ts
@@ -0,0 +1,53 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { apiUrl } from '../config/api-config';
+import { Observable } from 'rxjs';
+
+// Interfaz para proyectos
+export interface Proyecto {
+  id_proyecto?: number,
+  nombre: string,
+  descripcion?: string
+};
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ApiService {
+  private baseURL = apiUrl;
+
+  constructor(private http: HttpClient) {}
+
+  //POST: Crear un nuevo proyecto
+  enviarDatosProyectos(proyecto: Proyecto): Observable<any>{ 
+    return this.http.post(`${this.baseURL}/createproyecto`, proyecto);
+  }
+
+  //GET: Obtener un proyecto por su id
+  getProyecto(id_proyecto: number): Observable<Proyecto> {
+    return this.http.get<Proyecto>(`${this.baseURL}/getproyecto/${id_proyecto}`);
+  }
+
+  obtenerTodosProyectos(): Observable<Proyecto[]> {
+    return this.http.get<Proyecto[]>(`${this.baseURL}/getproyecto`);
+  }
+
+  //UPDATE: Modificar un proyecto
+  editarProyecto(proyecto: Proyecto): Observable<Proyecto[]> {
+    return this.http.put<Proyecto[]>(`${this.baseURL}/editproyecto`, proyecto);
+  }
+
+  //DELETE: Eliminar un proyecto
+  eliminarProyecto(id_proyecto: number): Observable<Proyecto> {
+    return this.http.delete<Proyecto>(`${this.baseURL}/deleteproyecto/${id_proyecto}`);
+  }
+
+  //POST: Crear una nueva alternativa
+  enviarDatosAlternativas(payload: any) {
+    return this.http.post(`${this.baseURL}/createalternativa`, payload);
+  }
+  //POST: Crear un nuevo criterio
+  enviarDatosCriterios(payload: any) {
+    return this.http.post(`${this.baseURL}/createcriterio`, payload);
+  }
+}
