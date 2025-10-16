2e5501aa8644076d2e8d7d9c2e31091a463ad067
Jona_Balles <JonatanBenjaminx7@gmail.com>
2025-10-15 17:11:55 -0700

    Update

--- Back/dist/db/connection.js
+++ Back/dist/db/connection.js
@@ -1,7 +1,7 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const sequelize_1 = require("sequelize");
-const sequelize = new sequelize_1.Sequelize("scoring_db", "root", "Horizon134", {
+const sequelize = new sequelize_1.Sequelize("scoring_db", "root", "Tinsel60285_", {
     //aqui cambia las credenciales de tu servidor de db,
     host: "localhost", //(nombre de db, usuario, contrasseña)
     dialect: "mysql",
